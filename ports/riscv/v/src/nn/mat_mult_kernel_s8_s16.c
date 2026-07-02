4710b1fcfc37ba6a752e323dbf50149599a20973
Harshit Kumar Shivhare <harshit.shivhare002@gmail.com>
2026-07-03 02:52:47 +0530

    riscv/v: clang-format mat_mult_kernel

--- ports/riscv/v/src/nn/mat_mult_kernel_s8_s16.c
+++ ports/riscv/v/src/nn/mat_mult_kernel_s8_s16.c
@@ -70,13 +70,13 @@ nn_mat_mult_kernel_s8_s16(const q7_t          *input_a,
         if (output_bias)
         {
             vint32m4_t vbias = __riscv_vle32_v_i32m4(bias_ptr, vl);
-            vacc_0 = vbias;
-            vacc_1 = vbias;
-            vacc_2 = vbias;
-            vacc_3 = vbias;
-            vacc_4 = vbias;
-            vacc_5 = vbias;
-            vacc_6 = vbias;
+            vacc_0           = vbias;
+            vacc_1           = vbias;
+            vacc_2           = vbias;
+            vacc_3           = vbias;
+            vacc_4           = vbias;
+            vacc_5           = vbias;
+            vacc_6           = vbias;
             bias_ptr += vl;
         }
         else
@@ -124,9 +124,11 @@ nn_mat_mult_kernel_s8_s16(const q7_t          *input_a,
         vint32m4_t vmult  = __riscv_vle32_v_i32m4(mult_ptr, vl);
         vint32m4_t vshift = __riscv_vle32_v_i32m4(shift_ptr, vl);
 
-        /* left_shift[i]  = max(shift[i], 0);  right_shift[i] = max(-shift[i], 0) */
-        vint32m4_t vleft_s  = __riscv_vmax_vx_i32m4(vshift, 0, vl);
-        vint32m4_t vright_s = __riscv_vmax_vx_i32m4(__riscv_vneg_v_i32m4(vshift, vl), 0, vl);
+        /* left_shift[i]  = max(shift[i], 0);  right_shift[i] = max(-shift[i],
+         * 0) */
+        vint32m4_t vleft_s = __riscv_vmax_vx_i32m4(vshift, 0, vl);
+        vint32m4_t vright_s
+            = __riscv_vmax_vx_i32m4(__riscv_vneg_v_i32m4(vshift, vl), 0, vl);
         vuint32m4_t vleft_u  = __riscv_vreinterpret_v_i32m4_u32m4(vleft_s);
         vuint32m4_t vright_u = __riscv_vreinterpret_v_i32m4_u32m4(vright_s);
 
@@ -140,22 +142,36 @@ nn_mat_mult_kernel_s8_s16(const q7_t          *input_a,
         vacc_6 = __riscv_vsll_vv_i32m4(vacc_6, vleft_u, vl);
 
         /* doubling_high_mult: round((acc * mult) >> 31) */
-        vint32m4_t vresult_0 = __riscv_vsmul_vv_i32m4(vacc_0, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_1 = __riscv_vsmul_vv_i32m4(vacc_1, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_2 = __riscv_vsmul_vv_i32m4(vacc_2, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_3 = __riscv_vsmul_vv_i32m4(vacc_3, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_4 = __riscv_vsmul_vv_i32m4(vacc_4, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_5 = __riscv_vsmul_vv_i32m4(vacc_5, vmult, __RISCV_VXRM_RNU, vl);
-        vint32m4_t vresult_6 = __riscv_vsmul_vv_i32m4(vacc_6, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_0
+            = __riscv_vsmul_vv_i32m4(vacc_0, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_1
+            = __riscv_vsmul_vv_i32m4(vacc_1, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_2
+            = __riscv_vsmul_vv_i32m4(vacc_2, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_3
+            = __riscv_vsmul_vv_i32m4(vacc_3, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_4
+            = __riscv_vsmul_vv_i32m4(vacc_4, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_5
+            = __riscv_vsmul_vv_i32m4(vacc_5, vmult, __RISCV_VXRM_RNU, vl);
+        vint32m4_t vresult_6
+            = __riscv_vsmul_vv_i32m4(vacc_6, vmult, __RISCV_VXRM_RNU, vl);
 
         /* divide_by_power_of_two: round(result >> right_shift) */
-        vresult_0 = __riscv_vssra_vv_i32m4(vresult_0, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_1 = __riscv_vssra_vv_i32m4(vresult_1, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_2 = __riscv_vssra_vv_i32m4(vresult_2, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_3 = __riscv_vssra_vv_i32m4(vresult_3, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_4 = __riscv_vssra_vv_i32m4(vresult_4, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_5 = __riscv_vssra_vv_i32m4(vresult_5, vright_u, __RISCV_VXRM_RNU, vl);
-        vresult_6 = __riscv_vssra_vv_i32m4(vresult_6, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_0
+            = __riscv_vssra_vv_i32m4(vresult_0, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_1
+            = __riscv_vssra_vv_i32m4(vresult_1, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_2
+            = __riscv_vssra_vv_i32m4(vresult_2, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_3
+            = __riscv_vssra_vv_i32m4(vresult_3, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_4
+            = __riscv_vssra_vv_i32m4(vresult_4, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_5
+            = __riscv_vssra_vv_i32m4(vresult_5, vright_u, __RISCV_VXRM_RNU, vl);
+        vresult_6
+            = __riscv_vssra_vv_i32m4(vresult_6, vright_u, __RISCV_VXRM_RNU, vl);
 
         /* Add output offset */
         vresult_0 = __riscv_vadd_vx_i32m4(vresult_0, out_offset, vl);
@@ -213,11 +229,12 @@ nn_mat_mult_kernel_s8_s16(const q7_t          *input_a,
         out_4 += vl;
         out_5 += vl;
         out_6 += vl;
-        mult_ptr  += vl;
+        mult_ptr += vl;
         shift_ptr += vl;
         ch_offset += vl;
-        remaining  -= vl;
+        remaining -= vl;
     }
 
-    return out_6; /* points one past the last written byte of the 7th output row */
+    return out_6; /* points one past the last written byte of the 7th output row
+                   */
 }
